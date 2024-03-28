cf6b9a65d1114b72e65eeab05d9fa2b71474ef06
kenduraghav <kenduraghav@gmail.com>
2024-03-28 22:55:18 +0800

    added binary and recursion example workouts

--- src/main/java/coding/problem/examples/recursions/DigitCounter.java
+++ src/main/java/coding/problem/examples/recursions/DigitCounter.java
@@ -3,7 +3,7 @@ package coding.problem.examples.recursions;
 public class DigitCounter {
 
 	public static void main(String[] args) {
-		System.out.println("The number of digits in the 'diameter of Mars': " + countDigits(7623));
+		System.out.println("The number of digits in the 'diameter of Mars': " + countDigits(764223));
 	}
 
 	static int countDigits(int num) {
@@ -11,7 +11,7 @@ public class DigitCounter {
 			return 0;
 		} else {
 			// Recursive case: reduce the number by dividing by 10 and count the rest
-			int lastDigit = num % 10;
+			int lastDigit = num % 10; // counting only the even number
 			if(lastDigit  % 2 == 0) {
 				return 1 + countDigits(num / 10);
 			} else {
