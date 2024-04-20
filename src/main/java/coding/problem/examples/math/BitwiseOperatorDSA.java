ce1d566608370ee0c3d3feeeb497fb9fb4613802
kenduraghav <kenduraghav@gmail.com>
2024-04-20 20:05:41 +0800

    Added bitwise manipulation / operations for DSA concepts

--- src/main/java/coding/problem/examples/math/BitwiseOperatorDSA.java
+++ src/main/java/coding/problem/examples/math/BitwiseOperatorDSA.java
@@ -3,10 +3,13 @@ package coding.problem.examples.math;
 public class BitwiseOperatorDSA {
 
 	public static void main(String[] args) {
-		System.out.println(isOdd(16));
+		System.out.println("Given no Is even ? " + isEven(16));
 		
-		int arr []  = {2,3,2,3,4,4,6,4,6,2,3,6,8};
-		System.out.println(findUnique(arr));
+		int arr []  = {2,3,4,6,4,2,3,6,8};
+		System.out.println("Two numbers Duplicate array " +findUnique(arr));
+		
+		int arr1 []  = {2,3,2,3,4,4,6,4,6,2,3,6,8};
+		System.out.println("Three numbers duplicate array :" +findUnique2(arr1));
 		
 		int n = 5;
 		System.out.println(findMagic(n));
@@ -24,16 +27,41 @@ public class BitwiseOperatorDSA {
 		return (n & 1) == 1;
 	}
 	
+	static boolean isEven(int n) {
+		return (n & 1) == 0;
+	}
+	
+	
 	static int findUnique(int [] arr) {
 		int result  = 0;
 		for(int i: arr) {
 			result ^=  i;
-			if(result / 3 == 0) {
-				result = i;
-			}
 		}
 		return result;
 	}
+	
+	
+	static int findUnique2(int[] arr) {
+        int ones = 0;
+        int twos = 0;
+
+        for (int num : arr) {
+            // Add the bits that are set in 'num' to 'ones' and 'twos' if they are already set in 'ones'
+        	
+            twos |= ones & num;
+
+            // XOR 'num' with 'ones' to find the bits that are set in 'num' but not in 'ones', then add them to 'ones'
+            ones ^= num;
+
+            // The bits that are set in both 'ones' and 'twos' represent elements that have appeared 3 times.
+            // We remove them from 'ones' and 'twos'
+            int commonBits = ones & twos;
+            ones &= ~commonBits;
+            twos &= ~commonBits;
+        }
+
+        return ones;
+    }
 	
 	
 	//Magic number  -> Powers of 5 or Sum of unique powers of 5. asked in Amazon
--- src/main/java/coding/problems/leetcode/assignments/KthMissingPostiveNumber.java
+++ src/main/java/coding/problems/leetcode/assignments/KthMissingPostiveNumber.java
@@ -3,20 +3,12 @@ package coding.problems.leetcode.assignments;
 public class KthMissingPostiveNumber {
 
 	public static void main(String[] args) {
-//		int arr [] = {1,2,3,4};
 		int arr [] = {2,3,4,7,11};
 		System.out.println(findKthPositive(arr, 5));
 	}
 	
 	static int findKthPositive(int[] arr, int k) {
-		
-//		for (int i = 0; i < arr.length; i++) {
-//			if (arr[i] <= k) {
-//				k++;
-//			}
-//		}
-//		return k;
-		 
+ 
 		int start = 0;
         int end = arr.length-1;
         while(start <= end){
